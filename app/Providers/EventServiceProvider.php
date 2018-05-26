e81b22244b62a676eadc0fedb2dd58bfc95533d0
Eduardo Aguad <eaguad@meat.cl>
2018-05-26 17:34:09 -0400

    [Events] Add approved/rejected events

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -13,8 +13,8 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        'App\Events\Event' => [
-            'App\Listeners\EventListener',
+        'EAguad\Events\OrderApprovedEvent' => [
+            'EAguad\Listeners\OrderApprovedListener',
         ],
     ];
 
---
+++ eaguad/Listeners/OrderApprovedListener.php
@@ -0,0 +1,29 @@
+<?php namespace EAguad\Listeners;
+
+use EAguad\Events\OrderApprovedEvent;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class OrderApprovedListener
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param OrderApprovedEvent $event
+     * @return void
+     */
+    public function handle(OrderApprovedEvent $event)
+    {
+        //
+    }
+}
