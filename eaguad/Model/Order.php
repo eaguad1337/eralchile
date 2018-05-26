b1ba1ff3b06b5494972a7ee13a3fa42b38b14f4e
Eduardo Aguad <eaguad@meat.cl>
2018-05-26 14:53:19 -0400

    [Order] Add model

---
+++ eaguad/Model/Order.php
@@ -0,0 +1,14 @@
+<?php namespace EAguad\Model;
+
+class Order extends Model
+{
+    protected $fillable = ['code', 'user_id'];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
